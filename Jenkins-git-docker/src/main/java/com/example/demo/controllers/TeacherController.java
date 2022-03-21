2dc0833530457509b364ff1d99242ce3970bc7ae
venkatesh A <venkiduen07@gmail.com>
2022-03-21 12:47:44 +0530

    new Changes

--- Jenkins-git-docker/src/main/java/com/example/demo/controllers/TeacherController.java
+++ Jenkins-git-docker/src/main/java/com/example/demo/controllers/TeacherController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +17,13 @@ public class TeacherController {
 		List<String> list = Arrays.asList("Alva","Anwar","Kamla");
 		return  list.toString();
 	}
+	@GetMapping(path="/teachers/{dept}")
+	public String getTeachersByDept(@PathVariable("dept") String dept) {
+		List<String> list = Arrays.asList("Alva","Anwar","Kamla");
+		if(dept.equals("ece")) {
+			list = Arrays.asList("Alva","Anwar","kanni");
+		}
+		return  list.toString();
+	}
+	
 }
